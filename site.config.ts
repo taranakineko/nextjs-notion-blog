f2ccf4babd0604c1e8070fe590fa25d8db874b0d
Taranaki <55310629+taranakineko@users.noreply.github.com>
2023-04-08 02:28:39 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,25 +2,25 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '9fc4005ace274416bf5d756d02c4700a',
+  rootNotionPageId: '4fb811a3549746cf956571ad713426ad',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
-  rootNotionSpaceId: "d438f9ea-c9ef-4898-ab16-bc9242467f4e",
+  rootNotionSpaceId: "",
 
   // basic site info (required)
-  name: 'lizhimiao的博客',
-  domain: 'lizhimiao.com',
-  author: 'zhimiao li',
+  name: "TaranakiNeko's Blog Bookmarks",
+  domain: 'bookmarks.nekoq.cyou',
+  author: 'TaranakiNeko',
 
   // open graph metadata (optional)
-  description: '笔记、博客和其他记录',
+  description: '这里是记录 Tara 曾经访问过的博客',
 
   // social usernames (optional)
-  twitter: '_lizhimiao',
-  github: 'zhimiaoli',
-  linkedin: 'lizhimiao',
-  // mastodon: '#', // optional mastodon profile URL, provides link verification
+  twitter: 'taranakineko',
+  github: 'taranakineko',
+  linkedin: '',
+  mastodon: 'https://alive.bar/@TaranakiNeko', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
@@ -34,7 +34,7 @@ export default siteConfig({
   // NOTE: this requires you to set up an external image proxy
 
   // Utteranc.es comments via GitHub issue comments (optional)
-  //utterancesGitHubRepo: "zhimiaoli/lizhimiao-dot-com-comment",
+  //utterancesGitHubRepo: "",
 
   // whether or not to enable support for LQIP preview images (optional)
   // NOTE: this requires you to set up Google Firebase and add the environment
@@ -50,11 +50,6 @@ export default siteConfig({
   //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
   // }
   pageUrlOverrides:   {
-    '/notion-as-blog': '2ac48272b7644cb0b0f8d19a104ddf05',
-    '/refers': '68892ddbd87341a5940b08fe9f3d079f',
-    '/digital-noting-tools-i-use': '7a26482a6a774a8dbd4872d9023b3957',
-    '/about': '0da3484664b44dcaa555509e3eb927f1',
-    '/archive': '299f3286fd3643ddbf6f4489b4813e81'
 },
 
   // whether to use the default notion navigation style or a custom one with links to
